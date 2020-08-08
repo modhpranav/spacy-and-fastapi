3dc14806e0820e335d37b002c1799ad2a1be01b3
pranav-modh <modhpranav21@gmail.com>
2020-08-09 02:08:48 +0530

    new html pages added with new phrases functionality

--- app/main.py
+++ app/main.py
@@ -1,21 +1,34 @@
-from typing import List
-from fastapi import FastAPI
+from typing import List, Any
+from fastapi import FastAPI, Request, Form
 from pydantic import BaseModel
-import en_core_web_sm
+import en_core_web_sm, pytextrank
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 
 nlp = en_core_web_sm.load()
 app = FastAPI()
+tr = pytextrank.TextRank()
+nlp.add_pipe(tr.PipelineComponent, name="textrank", last=True)
+templates = Jinja2Templates(directory="app/templates/")
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-
-class Response(BaseModel):
-    article_text: str = None
-
+@app.get("/")
+def get(request: Request):
+    return templates.TemplateResponse('dashboard.html', context={"request": request})
 
 @app.post("/")
-def main(input_data: Response):
-    result = []
-    if input_data.article_text:
-        doc = nlp(input_data.article_text)
-        for ent in doc.ents:
-            result.append({"text": ent.text, "label": ent.label_})
-    return result
+def post(request: Request, article: str = Form(...), operation: str = Form(...)):
+    labels = []
+    phrases = []
+    error_message = None
+    if article:
+        doc = nlp(article)
+        if operation == "label":
+            for ent in doc.ents:
+                labels.append({"text": ent.text, "label": ent.label_})
+        else:
+            phrases = list(filter(lambda x: len(str(x).split()) > 2, doc._.phrases))
+    else:
+        error_message = "Please try something else!"
+    return templates.TemplateResponse("result.html", context={"request": request , "labels": labels, "phrases": phrases, "error_message": error_message})
